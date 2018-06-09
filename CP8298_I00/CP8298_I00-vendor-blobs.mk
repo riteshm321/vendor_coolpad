0b116a00ea6fe438ffb17c737a070d9a836bb2d8
ChalapathiRevanth <princechalapathi16@gmail.com>
2018-06-09 04:30:20 +0000

    Vendor Blobs For Coolpad Note 3 & Lite

---
+++ CP8298_I00/CP8298_I00-vendor-blobs.mk
@@ -0,0 +1,16 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/app/mcRegistry,system/app/mcRegistry)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/vendor,system/vendor)
+
